df6e53d5e1ad17a6ac745cc48e33acce1d45f6e5
Brett Nelson <brett@WIPDeveloper.com>
2020-07-13 21:27:36 -0500

    set up config

---
+++ _data/sites/wipdeveloper.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'wipdeveloper.com', // optional, falls back to object key
+  description: 'Personal web site',
+  options: {
+    frequency: 60 * 23, // 23 hours
+    freshChrome: 'site',
+  },
+  urls: [
+    'https://wipdeveloper.com/',
+    'https://wipdeveloper.com/posts/',
+    'https://wipdeveloper.com/newsletter/',
+    'https://wipdeveloper.com/about/',
+    // Popular Posts
+    'https://wipdeveloper.com/lwc-loadscript-issue/',
+    'https://wipdeveloper.com/lwc-local-development-beta/',
+  ],
+};
