f17ea856de62432ec17a8a989f4c927df065ebad
Brett M. Nelson <brett@wipdeveloper.com>
2021-03-19 13:47:57 -0500

    add 3D Print Gallery

---
+++ _data/sites/3DPrint.gallery.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: '3dprint.gallery/', // optional, falls back to object key
+  description: 'WIPDeveloper.com',
+  options: {
+    frequency: 60 * 1, // 1 hours
+    freshChrome: 'site',
+  },
+  urls: [
+    'https://www.3dprint.gallery/',
+    'https://www.3dprint.gallery/what-i-use/',
+    'https://www.3dprint.gallery/prints/maker-robot/',
+  ],
+};
